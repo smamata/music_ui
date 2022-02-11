2ba3915186f3ddf35740cc33c589ac4491b1f8da
Mamata Shrestha <smamata321@gmail.com>
2022-02-11 14:50:12 +0545

    new

---
+++ lib/home.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class Music extends StatefulWidget {
+  const Music({Key? key}) : super(key: key);
+
+  @override
+  _MusicState createState() => _MusicState();
+}
+
+class _MusicState extends State<Music> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue.shade100,
+            ),
+          ),
+          Positioned(
+            top: 60,
+            left: 20,
+            child: Text(
+              "DANCE MONKEY",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.blue.shade900),
+            ),
+          ),
+          Positioned(
+            top: 120,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Tone and I",
+                  style: TextStyle(
+                      color: Colors.blue.shade800, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  "Abcd's Air Pods Pro",
+                  style: TextStyle(color: Colors.blue.shade200),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 220,
+            left: 40,
+            child: Container(
+              height: 330,
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade100,
+                  borderRadius: BorderRadius.circular(26),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 2, color: Colors.white70, spreadRadius: 5)
+                  ]),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
+                    child: Icon(
+                      Icons.data_saver_off_sharp,
+                      size: 30,
+                      color: Colors.blue.shade200,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
+                    child: Icon(
+                      Icons.skip_next_sharp,
+                      size: 30,
+                      color: Colors.blue.shade200,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                    child: Icon(
+                      Icons.pause,
+                      size: 30,
+                      color: Colors.blue.shade200,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
+                    child: Icon(
+                      Icons.skip_previous,
+                      size: 30,
+                      color: Colors.blue.shade200,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
+                    child: Icon(
+                      Icons.favorite,
+                      size: 30,
+                      color: Colors.red.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 170,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(blurRadius: 8, color: Colors.white, spreadRadius: 8)
+                ],
+              ),
+              child: CircleAvatar(
+                backgroundColor: Colors.white10,
+                radius: 180,
+                child: CircleAvatar(
+                  radius: 175,
+                  backgroundColor: Colors.blue.shade100,
+                  child: CircleAvatar(
+                    radius: 140,
+                    backgroundImage: AssetImage(
+                      "images/abcd.jpeg",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:music_ui/home.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Music(),
+  ));
+}
